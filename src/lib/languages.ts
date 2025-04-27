e8b80aab6bab8e5052fb39dc01ca8dedf6ba7cef
ennalyc <ecsl2@cin.ufpe.br>
2025-04-27 09:00:06 -0300

    first screens

---
+++ src/lib/languages.ts
@@ -0,0 +1,5 @@
+export const languages = [
+    { id: 1, language: "spanish", title: "Spanish", flag: "MX"},
+    { id: 2, language: "french", title: "French", flag: "FR" },
+    { id: 3, language: "chinese", title: "Chinese", flag: "CN"},
+  ]
